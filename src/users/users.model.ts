9f47998a490170a5def3805c8b062c97f03683cc
Pavel Krasovsky <frostcat2002@gmail.com>
2024-04-09 16:43:00 +0300

    Nest practice

---
+++ src/users/users.model.ts
@@ -0,0 +1,59 @@
+import { ApiProperty } from "@nestjs/swagger";
+import {
+  BelongsToMany,
+  Column,
+  DataType,
+  Model,
+  Table,
+} from "sequelize-typescript";
+import { Role } from "src/roles/roles.model";
+import { UserRoles } from "src/roles/user-roles.model";
+
+interface UserCreationAttrs {
+  email: string;
+  password: string;
+}
+
+@Table({ tableName: "users" })
+export class User extends Model<User, UserCreationAttrs> {
+  @ApiProperty({ example: "1", description: "Уникальный идентификатор" })
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
+
+  @ApiProperty({ example: "example@mail.com", description: "E-mail" })
+  @Column({
+    type: DataType.STRING,
+    unique: true,
+    allowNull: false,
+  })
+  email: string;
+
+  @ApiProperty({ example: "password", description: "Пароль" })
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  password: string;
+
+  @ApiProperty({ example: "false", description: "Забанен ли пользователь" })
+  @Column({
+    type: DataType.BOOLEAN,
+    defaultValue: false,
+  })
+  banned: boolean;
+
+  @ApiProperty({ example: "Причина", description: "Причина блокировки" })
+  @Column({
+    type: DataType.STRING,
+    allowNull: true,
+  })
+  banReason: string;
+
+  @BelongsToMany(() => Role, () => UserRoles)
+  roles: Role[];
+}
